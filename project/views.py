d8d4305ff6c89472243d0935ab05be5459259aca
aacxx <a08001212@gmail.con>
2024-01-11 18:08:32 +0800

    fix wrong filename extension.

--- project/views.py
+++ project/views.py
@@ -1,3 +1,4 @@
+from anyio import sleep
 from django.shortcuts import render, redirect,get_object_or_404
 from django.http import HttpResponse, FileResponse
 from django.template import loader
@@ -5,6 +6,10 @@ from django import forms
 import yt_dlp
 import base64
 from pathlib import Path
+from os import listdir
+import threading
+
+
 
 
 def test(request):
@@ -32,18 +37,27 @@ def test(request):
 
 class URLForm(forms.Form):
     # Define your form fields here
-    url = forms.CharField(label='URL', max_length=1000)
+    url = forms.CharField(label='URL', max_length=10000)
 
 def download(url: str):
     config = dict()
+    path = "project/static/"
+    full_file_name = "Not found."
     # get title
     with yt_dlp.YoutubeDL() as dlp:
         title = dlp.sanitize_info(dlp.extract_info(url, download=False))['title']
-        file = str(base64.b64encode(bytes(title, encoding='utf-8')))[2:]
+        file = str(hash(url))
         config['outtmpl'] = {'default': f'project/static/{file}'}
 
     # download
     with yt_dlp.YoutubeDL(config) as dlp:
         dlp.download([url])
-    return [f"{file}.webm", f"{title}.webm"]
+
+    # get filename and filename extension
+    files = listdir(path)
+    for f in files:
+        if file == f[:len(file)]:
+            full_file_name = f
+
+    return [f"{full_file_name}", f"{title}.webm"]
 
